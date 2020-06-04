503ad6e36fc4be6f8c2b39deb27bed6ab8ff8260
Flora Petersen <floramarina@comcast.net>
2020-06-04 19:34:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
 def oxford_comma(array)
   if array.size == 1
     return array.join
-  end
-  if array.size == 2
+  elsif array.size == 2
     return array.join(" and ")
+  elsif array.size > 2
+    array.insert(-2, " and ")
+    return array.join(",")
   end
 end
+
+    
+  
