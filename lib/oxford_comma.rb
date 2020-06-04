6db066213303bb53a6684bccbd7cc00fe20ebd36
Flora Petersen <floramarina@comcast.net>
2020-06-04 20:29:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 2
     return array.join(" and ")
   elsif array.size > 2
-    array[1].prepend("and")
+    array[-1].prepend("and")
     array.join(", ")
   else
     array.join
